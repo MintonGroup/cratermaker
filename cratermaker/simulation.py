907278d2d2cc9550a75333a998be872c27c1112b
David Minton <daminton@purdue.edu>
2023-11-02 09:07:40 -0400

    Fixed issues with mesh file systems

--- cratermaker/simulation.py
+++ cratermaker/simulation.py
@@ -3,6 +3,7 @@ import xarray as xr
 import numpy as np
 import jigsawpy
 import os
+from numpy.random import default_rng
 
 # Define some basic parameters
 body_radius_values = {
@@ -16,23 +17,30 @@ class Simulation(object):
     This is a class that defines the basic Cratermaker surface object. 
     """
     def __init__(self):
-        # TODO: Initialize with configure options or read in configuration file
         self.cwd = os.getcwd()
         
+        # Set default configuration options
+        # TODO: Initialize with configure options as arguments or read in configuration file
         self.config = {
             "body" : "Moon", # Which body to simulation (Options are "Moon","Custom" )
             "pix"  : 6.16e3, # Approximate cell global cell size
             "cachedir" : os.path.join(self.cwd,".cache"), # Directory location of output files
             "meshname" : "body", # Name of files generated during mesh process
+            "seed" : 5421109845, # RNG seed
         }
+        
+        # Determine the radius of the target body
         if self.config['body'] != 'Custom':
             self.config['body_radius'] = body_radius_values[self.config['body']]
            
+        # Set up Jigsaw mesh files and mesh body if a file doesn't already exist
         self.meshfile = os.path.join(self.config['cachedir'],f"{self.config['meshname']}.msh")
         if not os.path.exists(self.config['cachedir']):
             os.mkdir(self.config['cachedir'])
         if not os.path.exists(self.meshfile):
             self.mesh_body()   
+            
+        self.rng = default_rng(seed=self.config['seed'])
         # self._surface = _SurfaceBind(gridshape)     
         # lat = np.linspace(-90, 90, gridshape[0])
         # lon = np.linspace(0, 360, gridshape[1])
@@ -58,16 +66,17 @@ class Simulation(object):
 
     def mesh_body(self):
         # This will get updated evantually after we're done testing
-        input_name='ctem_body'
+        meshname=self.config['meshname']
+        cachedir=self.config['cachedir']
         # Set up jigsaw objects
         opts = jigsawpy.jigsaw_jig_t()
         geom = jigsawpy.jigsaw_msh_t()
         mesh = jigsawpy.jigsaw_msh_t()
 
         # Define file names
-        opts.geom_file = f"{input_name}_base.msh"
-        opts.jcfg_file = f"{input_name}.jig"
-        opts.mesh_file = f"{input_name}.msh"
+        opts.geom_file = os.path.join(cachedir,f"{meshname}_base.msh")
+        opts.jcfg_file = os.path.join(cachedir,f"{meshname}.jig")
+        opts.mesh_file = os.path.join(cachedir,f"{meshname}.msh")
 
         # Define basic sphere using the built-in ellipsoid mesh model
         geom.mshID = "ellipsoid-mesh"
@@ -90,6 +99,6 @@ class Simulation(object):
             mesh.point["coord"],
             mesh.tria3["index"])
 
-        jigsawpy.savevtk(f"{input_name}.vtk", mesh)
+        jigsawpy.savevtk(os.path.join(cachedir,f"{meshname}.vtk"), mesh)
         self.mesh = mesh
         return
