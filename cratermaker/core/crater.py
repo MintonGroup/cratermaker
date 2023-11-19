49ed041932cc7b5abe218b8d39b0a09b3ab2802f
David Minton <daminton@purdue.edu>
2023-11-19 15:25:12 -0500

    Added crater scaling to its own modeule and updated the scaling stuff to the Crater object constructor

--- cratermaker/core/crater.py
+++ cratermaker/core/crater.py
@@ -1,6 +1,9 @@
-from dataclasses import dataclass, field
 import numpy as np
+from numpy.random import Generator
+from dataclasses import dataclass, field
 from ..models.production_function import ProductionFunction
+from ..models import crater_scaling 
+from ..core.target import Target
 @dataclass
 class Crater:
     """
@@ -20,8 +23,14 @@ class Crater:
     transient_radius: np.float64 = field(default=None)
     location: np.ndarray = field(default=None)
     
-    def __post_init__(self):
-        values_set = sum(x is not None for x in [self.production, self.diameter, self.radius, 
+    def __post_init__(self, target: Target, rng: Generator=None):
+        if not isinstance(target, Target):
+            raise TypeError("target must be an instance of Target")
+        if rng and not isinstance(rng, Generator):
+            raise TypeError("The 'rng' argument must be a numpy.random.Generator instance or None")
+        if rng is None:
+            rng = np.random.default_rng()    
+            values_set = sum(x is not None for x in [self.production, self.diameter, self.radius, 
                                                  self.transient_diameter, self.transient_radius])
 
         if values_set > 1:
@@ -29,13 +38,24 @@ class Crater:
 
         if self.diameter is not None:
             self.radius = self.diameter / 2
+            self.final_to_transient(target,rng)
         elif self.radius is not None:
             self.diameter = self.radius * 2
+            self.final_to_transient(target,rng)
         elif self.transient_diameter is not None:
             self.transient_radius = self.transient_diameter / 2
+            self.transient_to_final(target,rng)
         elif self.transient_radius is not None:
             self.transient_diameter = self.transient_radius * 2
+            self.transient_to_final(target,rng)
 
+        if self.morphology_type is None:
+            transition_diameter, = crater_scaling.get_simple_to_complex_transition_factors(target,rng)
+            if self.diameter < transition_diameter:
+                self.morphology_type = "simple" 
+            else:
+                self.morphology_type = "complex"
+                
         if self.location is not None:
             if not isinstance(self.location, np.ndarray):
                 self.location = np.array(self.location, dtype=np.float64)
@@ -43,4 +63,31 @@ class Crater:
                 raise ValueError("location must be a 2-element array")
         
         return
+    
+
+    def final_to_transient(self, target: Target, rng: Generator=None):
+        if not isinstance(target, Target):
+            raise TypeError("target must be an instance of Target")
+        if rng and not isinstance(rng, Generator):
+            raise TypeError("The 'rng' argument must be a numpy.random.Generator instance or None")
+        if rng is None:
+            rng = np.random.default_rng() 
+        self.transient_diameter = crater_scaling.final_to_transient(self.diameter,target,rng)
+        self.transient_radius = self.transient_diameter / 2
+        return 
+
+
+    def transient_to_final(self, target: Target, rng: Generator=None):
+        if not isinstance(target, Target):
+            raise TypeError("target must be an instance of Target")
+        if rng and not isinstance(rng, Generator):
+            raise TypeError("The 'rng' argument must be a numpy.random.Generator instance or None")
+        if rng is None:
+            rng = np.random.default_rng()
+        self.diameter, self.morphology_type = crater_scaling.transient_to_final(self.transient_diameter,target,rng)    
+        self.radius = self.diameter / 2
+        return 
+
+
+     
    
---
+++ cratermaker/models/crater_scaling.py
@@ -0,0 +1,85 @@
+import numpy as np
+from numpy.random import Generator
+from ..core.crater import Crater   
+from ..core.target import Target
+
+def get_simple_to_complex_transition_factors(target: Target, rng: Generator=None):
+   if not isinstance(target, Target):
+      raise TypeError("target must be an instance of Target")
+   if rng and not isinstance(rng, Generator):
+      raise TypeError("The 'rng' argument must be a numpy.random.Generator instance or None")
+   if rng is None:
+      rng = np.random.default_rng() 
+        
+   simple_enlargement_factor = 0.84 # See Melosh (1989) pg. 129 just following eq. 8.2.1
+   transition_exp_mean = 0.15 # See Croft (1985) eq. 9
+   final_exp_mean = 0.85      # See Croft (1985) eq. 9
+   # Standard deviations for the exponents
+   transition_exp_std = 0.04
+   final_exp_std = 0.04
+   
+   # The transition values come from CTEM and are a synthesis of Pike (1980), Croft (1985), Schenk et al. (2004).
+   if target.transition_scale_type == "silicate":
+      simple_complex_exp = -1.0303 
+      simple_complex_mean = 2*16533.8 
+      simple_complex_std = 0.05
+   elif target.transition_scale_type == "ice":
+      simple_complex_exp = -1.22486
+      simple_complex_mean = 2*3081.39
+      simple_complex_std = 0.05
+      
+   # Draw from a normal distribution for each exponent
+   transition_exp = rng.normal(transition_exp_mean, transition_exp_std)
+   final_exp = rng.normal(final_exp_mean, final_exp_std)
+   simple_complex_fac = simple_complex_mean * np.exp(rng.normal(scale=simple_complex_std))
+   transition_diameter = simple_complex_fac * target.gravity**simple_complex_exp
+   
+   return transition_diameter, simple_enlargement_factor, transition_exp, final_exp 
+
+
+def final_to_transient(final_diameter, target: Target, rng: Generator=None):
+   if not isinstance(target, Target):
+      raise TypeError("target must be an instance of Target")
+   if rng and not isinstance(rng, Generator):
+      raise TypeError("The 'rng' argument must be a numpy.random.Generator instance or None")
+   if rng is None:
+      rng = np.random.default_rng()    
+   transition_diameter, simple_enlargement_factor, transition_exp, final_exp = get_simple_to_complex_transition_factors(target,rng) 
+   if final_diameter < transition_diameter:
+      transient_diameter = simple_enlargement_factor * final_diameter  # Simple crater scaling
+   else:
+      transient_diameter = 1e3 * (transition_diameter * 1e-3)**transition_exp * (final_diameter * 1e-3)**final_exp # Complex crater scaling (in m)
+   
+   return np.float64(transient_diameter)
+
+
+def transient_to_final(transient_diameter, target: Target, rng: Generator=None):
+   if not isinstance(target, Target):
+      raise TypeError("target must be an instance of Target")
+   if rng and not isinstance(rng, Generator):
+      raise TypeError("The 'rng' argument must be a numpy.random.Generator instance or None")
+   if rng is None:
+      rng = np.random.default_rng() 
+           
+   transition_diameter, simple_enlargement_factor, transition_exp, final_exp = get_simple_to_complex_transition_factors(target,rng)
+   
+   final_diameter_simple = np.float64(transient_diameter / simple_enlargement_factor)
+   final_diameter_complex = np.float64(1e3 * ((1e-3 * transient_diameter) / (transition_diameter * 1e-3)**transition_exp)**(1.0 / final_exp))
+   
+   if final_diameter_simple < transition_diameter and final_diameter_complex < transition_diameter: # Unambiguosly simple
+      final_diameter = final_diameter_simple
+      morphology_type = "simple"
+   elif final_diameter_simple > transition_diameter and final_diameter_complex > transition_diameter: # Unambiguously complex
+      final_diameter = final_diameter_complex
+      morphology_type = "complex"
+   else: # Could be either complex or simple. We'll just draw which one from a hat weighted in the direction of whichever size is closest to the transition
+      is_simple = rng.random() < np.abs(final_diameter_complex - transition_diameter) / np.abs(final_diameter_simple - final_diameter_complex)
+      morphology_type = rng.choice(["simple", "complex"])
+      if is_simple:
+            final_diameter = final_diameter_simple
+            morphology_type = "simple"
+      else:
+            final_diameter = final_diameter_complex
+            morphology_type = "complex"
+      
+   return final_diameter, morphology_type   
