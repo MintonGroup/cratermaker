0c8148e078fe5bb134446bd9418fd6038d20eb88
David Minton <daminton@purdue.edu>
2023-11-30 10:57:45 -0500

    Big restructuring to move a bunch of mesh things out of Target and into Simulation and a new mesh_tools utility module

--- cratermaker/core/simulation.py
+++ cratermaker/core/simulation.py
@@ -5,35 +5,85 @@ from dacite import from_dict
 import xarray as xr
 from typing import Any
 import os
+from pathlib import Path
 from .target import Target
 from .material import Material
 from .projectile import Projectile
 from .crater import Crater 
-from ..utils import general_utils  as gu
+from ..utils import general_utils as gu
+from ..utils.general_utils import float_like
+from ..utils import mesh_tools
 from . import montecarlo as mc
 from ..models import craterscaling as cs 
 
 class Simulation():
     """
-    This is a class that defines the basic Cratermaker body object. 
+    This is a class that defines the Simulation object in Cratermaker, which orchestrates the processes involved in running a simulation. 
+    
+ 
+    Attributes
+    ----------
+    mesh_temp_dir : str, Path, or os.PathLike or None
+        Directory path for caching data.
+    mesh_file : str, Path, or os.PathLike or None
+        File path for the dataset file describing the target mesh.
+    dem_file : str, Path, or os.PathLike or None
+        File path for the dataset file describing the target elevation data. 
+    ds : xarray.Dataset
+        xarray Dataset representing the surface mesh and associated data.    
+    pix : float_like or None
+        Pixel resolution for the mesh.
+        
+        
     """
     def __init__(self, 
                 target_name: str="Moon",
                 material_name: str | None = None,
-                make_new_surface:  bool | None = None,
+                make_new_mesh:  bool | None = None,
+                reset_surface: bool = True,
+                mesh_temp_dir: str | Path | os.PathLike = ".mesh",
+                mesh_file: str | Path | os.PathLike = "surface_mesh.nc",
+                dem_file: str | Path | os.PathLike = "surface_dem.nc",
+                pix: float_like | None = None,
+                mesh_data: xr.Dataset | None=None,
+                dem_data: xr.Dataset | None=None,
                 **kwargs: Any):
+      
+        self.mesh_temp_dir = Path(mesh_temp_dir)
+        self.mesh_file = Path(mesh_file)
+        if not self.mesh_file.is_absolute():
+            self.mesh_file = Path.cwd() / self.mesh_file
+            
+        self.dem_file = Path(dem_file)
+        if not self.dem_file.is_absolute():
+            self.dem_file = Path.cwd() / self.dem_file
+            
         if material_name:
             material = Material(name=material_name)
             self.target = from_dict(data_class=Target,data=dict({"name":target_name,"material":material}, **kwargs)) 
         else: 
-            self.target = from_dict(data_class=Target,data=dict({"name":target_name}, **kwargs)) 
-    
+            self.target = from_dict(data_class=Target,data=dict({"name":target_name}, **kwargs))
+            
+        if pix is not None:
+            self.pix = np.float64(pix)
+        else:    
+            self.pix = np.sqrt(4 * np.pi * self.target.radius**2) * 1e-3  # Default mesh scale that is somewhat comparable to a 1000x1000 CTEM grid
+                
+        if not os.path.exists(self.mesh_temp_dir):
+            os.mkdir(self.mesh_temp_dir)
+
         # Generate a new surface if either it is explicitly requested via parameter or a data file doesn't yet exist 
-        if make_new_surface is None:
-            make_new_surface = not os.path.exists(self.target.ds_file)
+        if make_new_mesh is None:
+            make_new_mesh = not os.path.exists(self.mesh_file)
         
-        if make_new_surface:    
-            self.target.make_new_surface()
+        if make_new_mesh:
+            self.mesh = mesh_tools.generate(self.target.radius,self.pix,self.mesh_file,self.mesh_temp_dir)
+            reset_surface = True
+        else:
+            self.mesh = xr.open_dataset(self.mesh_file)
+            
+        if reset_surface:
+            self.dem = mesh_tools.set_elevation(nCells=self.mesh.nCells.size)
             
         # Set some default values for the simulation parameters
         self.time_function = kwargs.get('time_function', None)
@@ -52,9 +102,50 @@ class Simulation():
         self._projectile = None
 
         return
+
     
+    def set_properties(self, **kwargs):
+        """
+        Set properties of the current object based on the provided keyword arguments.
+
+        This function is a utility to update the properties of the current object. The actual implementation of the 
+        property setting is handled by the `utils.gu.set_properties` method.
+
+        Parameters
+        ----------
+        **kwargs : dict
+            A dictionary of keyword arguments that represent the properties to be set on the current object.
+
+        Returns
+        -------
+        None
+            The function does not return a value.
+        """        
+        gu.set_properties(self,**kwargs)
+        return 
+
+    
+    def to_json(self, filename):
+        #TODO: Re-do this once the dust settles a bit
+        # Get the simulation configuration into the correct structure
+        material_config = gu.to_config(self.target.material)
+        target_config = {**gu.to_config(self.target), 'material' : material_config}
+        sim_config = {**gu.to_config(self),'target' : target_config} 
+        
+        # Write the combined configuration to a JSON file
+        with open(filename, 'w') as f:
+            json.dump(sim_config, f, indent=4)
             
-    def add_crater(self, **kwargs):
+        return
+    
+
+
+
+
+
+
+            
+    def generate_crater(self, **kwargs):
         # Create a new Crater object with the passed arguments and set it as the crater of this simulation
         crater = Crater(**kwargs)
         
@@ -84,8 +175,8 @@ class Simulation():
         
         return
     
-
-    def add_projectile(self, **kwargs):
+    
+    def generate_projectile(self, **kwargs):
         # Create a new Crater object with the passed arguments and set it as the crater of this simulation
         projectile = Projectile(**kwargs)
         
@@ -124,57 +215,39 @@ class Simulation():
             
         # Now add the crater that this projectile would make
         transient_diameter, _ = cs.projectile_to_transient(projectile, self.target, self.rng) 
-        self.add_crater(transient_diameter=transient_diameter,location=projectile.location)
+        self.generate_crater(transient_diameter=transient_diameter,location=projectile.location)
         self.projectile = projectile
         
         return
    
-    
-    def to_json(self, filename):
+   
+    def emplace_crater(self, from_projectile=False, **kwargs):
+        if from_projectile:
+            self.generate_projectile(**kwargs)
+        else:
+            self.generate_crater(**kwargs)
+        self.mesh['crater_distance'] = mesh_tools.get_cell_distance(self.mesh, self.crater.location, self.target.radius)
+        self.mesh['crater_bearing'] = mesh_tools.get_cell_initial_bearing(self.mesh, self.crater.location)
         
-        # Get the simulation configuration into the correct structure
-        material_config = gu.to_config(self.target.material)
-        target_config = {**gu.to_config(self.target), 'material' : material_config}
-        sim_config = {**gu.to_config(self),'target' : target_config} 
+        self.crater.average_surface_normal_vector, self.crater.average_surface_radius = mesh_tools.get_average_surface(self.mesh, self.dem, self.crater.location, self.crater.radius)
         
-        # Write the combined configuration to a JSON file
-        with open(filename, 'w') as f:
-            json.dump(sim_config, f, indent=4)
-        
-    @property
-    def elevation(self):
-        return self._body.fobj.elevation
+        return  
     
-    @property
-    def name(self):
-        return self._body.fobj.name
 
 
-    def get_elevation(self):
-        return self._body.get_elevation()
-
+    # The following are placeholders for if/when we need to pass data back and forth to the Fortran library     
+    # @property
+    # def elevation(self):
+    #     return self._body.fobj.elevation
     
-    def set_elevation(self, elevation_array):
-        self._body.set_elevation(elevation_array)
+    # @property
+    # def name(self):
+    #     return self._body.fobj.name
+
+
+    # def get_elevation(self):
+    #     return self._body.get_elevation()
 
     
-    def set_properties(self, **kwargs):
-        """
-        Set properties of the current object based on the provided keyword arguments.
-
-        This function is a utility to update the properties of the current object. The actual implementation of the 
-        property setting is handled by the `utils.gu.set_properties` method.
-
-        Parameters
-        ----------
-        **kwargs : dict
-            A dictionary of keyword arguments that represent the properties to be set on the current object.
-
-        Returns
-        -------
-        None
-            The function does not return a value.
-        """        
-        gu.set_properties(self,**kwargs)
-        return 
-
+    # def set_elevation(self, elevation_array):
+    #     self._body.set_elevation(elevation_array)
--- cratermaker/core/target.py
+++ cratermaker/core/target.py
@@ -4,9 +4,7 @@ import xarray as xr
 from pathlib import Path
 from dataclasses import dataclass, field, asdict
 from typing import Tuple
-from operator import sub
 import os
-from mpas_tools.mesh.creation.build_mesh import build_spherical_mesh
 from .material import Material
 from ..utils.general_utils import set_properties, create_catalogue, check_properties, float_like
 
@@ -34,18 +32,10 @@ class Target:
         Material composition of the target body.
     mean_impact_velocity : float_like or None
         Mean impact velocity in m/s.
-    pix : float_like or None
-        Pixel resolution for the mesh.
     transition_scale_type : str or None
         Simple-to-complex transition scaling to use for the surface (either "silicate" or "ice").
-    cachedir : str, Path, or os.PathLike or None
-        Directory path for caching data.
-    ds_file : str, Path, or os.PathLike or None
-        File path for the target dataset file.
     catalogue : dict or None
         Optional input of catalogue solar system targets to replace the built-in catalogue.
-    ds : xarray.Dataset
-        xarray Dataset representing the surface mesh and associated data.
     """
        
     # Set up instance variables
@@ -56,19 +46,8 @@ class Target:
     material_name: str | None = None
     material: Material | None = None
     mean_impact_velocity: float_like | None = None
-    pix: float_like | None = None
     transition_scale_type: str | None = None
-    cachedir: str | Path | os.PathLike | None = None
-    ds_file: str | Path | os.PathLike | None = None
     catalogue: dict | None = None
-    ds: xr.Dataset = field(default_factory=xr.Dataset)
-    
-    def __getitem__(self,key):
-        return self.ds[key]
-    
-    def __setitem__(self, key, value):
-        self.ds[key] = value
-        return
     
     config_ignore = ['catalogue','material']  # Instance variables to ignore when saving to file
     def __post_init__(self):
@@ -97,19 +76,6 @@ class Target:
        
         if self.catalogue is None: 
             self.catalogue = create_catalogue(body_properties, body_values)
-
-        if self.cachedir is None:
-            self.cachedir = Path.cwd() / ".cache" 
-        elif not isinstance(self.cachedir, Path):
-            self.cachedir = Path(self.cachedir)
-            
-        if self.ds_file is None:
-            self.ds_file = Path.cwd() / "surface_dem.nc"
-        elif not isinstance(self.ds_file, Path):
-            self.ds_file = Path(self.ds_file)
-            
-        if not os.path.exists(self.cachedir):
-            os.mkdir(self.cachedir)
         
         # ensure that only either diamter of radius is passed
         values_set = sum(x is not None for x in [self.diameter, self.radius])
@@ -135,21 +101,13 @@ class Target:
         elif self.radius is not None:
             self.radius = np.float64(self.radius)
             self.diameter = self.radius * 2       
-            
         if self.radius is not None:
             self.radius = np.float64(self.radius)
         if self.gravity is not None:
             self.gravity = np.float64(self.gravity)
         if self.mean_impact_velocity is not None:
             self.mean_impact_velocity = np.float64(self.mean_impact_velocity)
-        if self.pix is not None:
-            self.pix = np.float64(self.pix)
-        else:    
-            self.pix = np.sqrt(4 * np.pi * self.radius**2) * 1e-3  # Default mesh scale that is somewhat comparable to a 1000x1000 CTEM grid
-            
-        if os.path.exists(self.ds_file):
-            self.ds = xr.open_dataset(self.ds_file)
-            
+
         valid_transition_scale_types = ["silicate", "ice"]
         if self.transition_scale_type is not None:
             if not isinstance(self.transition_scale_type, str):
@@ -163,56 +121,6 @@ class Target:
         
         return
     
-
-    def make_new_surface(self):
-        """
-        Generate a tessellated mesh of a sphere using the jigsawpy library and convert it to xarray format.
-
-        This function sets up Jigsaw mesh files and a mesh body, defines a basic sphere using an ellipsoid mesh model,
-        sets mesh options, generates a tessellated mesh, and converts the mesh to an xarray Dataset. 
-
-        Returns
-        -------
-        None
-            The function does not return a value but updates the `self.mesh` attribute with the generated uxarray object.
-        """
-        def cellWidthVsLatLon(pix):
-            """
-            Create cell width array for this mesh on a regular latitude-longitude grid.
-            Returns
-            -------
-            cellWidth : ndarray
-                m x n array of cell width in km
-            lon : ndarray
-                longitude in degrees (length n and between -180 and 180)
-            lat : ndarray
-                longitude in degrees (length m and between -90 and 90)
-            """
-            dlat = 10
-            dlon = 10
-            constantCellWidth = pix * 1e-3 # build_spherical_mesh assumes units of km, so must be converted
-
-            nlat = int(180/dlat) + 1
-            nlon = int(360/dlon) + 1
-
-            lat = np.linspace(-90., 90., nlat)
-            lon = np.linspace(-180., 180., nlon)
-
-            cellWidth = constantCellWidth * np.ones((lat.size, lon.size))
-            return cellWidth, lon, lat
-        
-        cellWidth, lon, lat = cellWidthVsLatLon(self.pix)
-        orig_dir = os.getcwd()
-        os.chdir(self.cachedir)
-        build_spherical_mesh(cellWidth, lon, lat, out_filename=str(self.ds_file), earth_radius=self.radius, plot_cellWidth=False)
-        os.chdir(orig_dir)
-    
-        self.ds = xr.load_dataset(self.ds_file)
-        self.set_elevation()
-        self.ds.to_netcdf(self.ds_file)
-        
-        return 
-        
         
     def set_elevation(self,new_elev=None):
         """
@@ -231,7 +139,7 @@ class Target:
             dims=["nCells"],
             attrs={"long_name":"elevation of cells"}
             )
-        self.ds['elevation'] = dem 
+        
         return
     
     
@@ -267,170 +175,3 @@ class Target:
             Escape velocity in m/s.
         """        
         return np.sqrt(2 * self.radius * self.gravity)
-
-    @staticmethod
-    def calculate_haversine_distance(lon1: float_like, 
-                    lat1: float_like, 
-                    lon2: float_like, 
-                    lat2: float_like,
-                    radius: float_like) -> np.float64:
-        """
-        Calculate the great circle distance between two points on a sphere.
-
-        Parameters
-        ----------
-        lon1 : float_like
-            Longitude of the first point in radians.
-        lat1 : float_like
-            Latitude of the first point in radians.
-        lon2 : float_like
-            Longitude of the second point in radians.
-        lat2 : float_like
-            Latitude of the second point in radians.
-        radius : float_like
-            Radius of the sphere in meters.
-
-        Returns
-        -------
-        np.float64
-            Great circle distance between the two points in meters.
-        """
-        # Calculate differences in coordinates
-        dlon = lon2 - lon1
-        dlat = lat2 - lat1
-
-        # Haversine formula
-        a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
-        c = 2 * np.arcsin(np.sqrt(a))
-        return radius * c
-    
-    
-    def get_cell_distance(self, location: Tuple[np.float64, np.float64]) -> xr.DataArray:
-        """
-        Computes the distances between cell centers and a given location.
-
-        Parameters
-        ----------
-        location : Tuple[np.float64, np.float64]
-            Tuple containing the longitude and latitude of the location in radians.
-
-        Returns
-        -------
-        xarray.DataArray
-            DataArray of distances for each cell in meters.
-        """
-        return self.calculate_haversine_distance(location[0],location[1],self.ds.lonCell,self.ds.latCell,self.radius)
-    
-    
-    @staticmethod 
-    def calculate_initial_bearing(lon1: float_like, 
-                                lat1: float_like, 
-                                lon2: float_like, 
-                                lat2: float_like) -> np.float64:
-        """
-        Calculate the initial bearing from one point to another on the surface of a sphere.
-
-        Parameters
-        ----------
-        lon1 : float_like
-            Longitude of the first point in radians.
-        lat1 : float_like
-            Latitude of the first point in radians.
-        lon2 : float_like
-            Longitude of the second point in radians.
-        lat2 : float_like
-            Latitude of the second point in radians.
-
-        Returns
-        -------
-        np.float64
-            Initial bearing from the first point to the second point in radians.
-        """
-        # Calculate differences in coordinates
-        dlon = np.mod(lon2 - lon1 + np.pi, 2 * np.pi) - np.pi
-
-        # Haversine formula calculations
-        x = np.sin(dlon) * np.cos(lat2)
-        y = np.cos(lat1) * np.sin(lat2) - np.sin(lat1) * np.cos(lat2) * np.cos(dlon)
-        initial_bearing = np.arctan2(x, y)
-
-        # Normalize bearing to 0 to 2*pi
-        initial_bearing = (initial_bearing + 2 * np.pi) % (2 * np.pi)
-
-        return initial_bearing
-
-    
-    def get_cell_initial_bearing(self, location: Tuple[np.float64, np.float64]) -> xr.DataArray:
-        """
-        Computes the initial bearing between cell centers and a given location.
-
-        Parameters
-        ----------
-        location : Tuple[np.float64, np.float64]
-            Tuple containing the longitude and latitude of the location in radians.
-
-        Returns
-        -------
-        xarray.DataArray
-            DataArray of initial bearings for each cell in radians.
-        """
-        return self.calculate_initial_bearing(location[0], location[1], self.ds.lonCell, self.ds.latCell)
-    
-
-    def get_average_surface(self, location: Tuple[np.float64, np.float64], radius: np.float64) -> Tuple[np.float64, np.float64]:
-        """
-        Calculate the orientation and radius of the hemispherical cap.
-
-        Parameters
-        ----------
-        location : Tuple[float, float]
-            Tuple containing the longitude and latitude of the reference location in radians.
-        radius : float
-            The reference radius of to compute the average over in meters.
-
-        Returns
-        -------
-        cap_center_vector : ndarray
-            The vector pointing to the center of the cap from the sphere's center.
-        cap_radius : float
-            The radius of the cap.
-        """
-        # Calculate distances from the crater center to each cell
-        distances = self.get_cell_distance(location)
-
-        # Find cells within the crater radius
-        cells_within_radius = distances <= radius
-
-        # Get the bearings and distances for cells within the crater radius
-        bearings = self.ds['bearing'].where(cells_within_radius, drop=True)
-        distances = distances.where(cells_within_radius, drop=True)
-
-        # Convert bearings to vector components
-        # Bearing is angle from north, positive clockwise, but we need standard mathematical angle, positive counter-clockwise
-        angles = np.deg2rad(90) - bearings  # Convert bearing to angle in radians
-        x_components = np.cos(angles) * distances
-        y_components = np.sin(angles) * distances
-
-        # Calculate the weighted average vector components
-        # Weight by the area of each cell to give more importance to larger cells
-        cell_areas = self.ds['areaCell'].where(cells_within_radius, drop=True)
-        weighted_x = (x_components * cell_areas).sum() / cell_areas.sum()
-        weighted_y = (y_components * cell_areas).sum() / cell_areas.sum()
-
-        # Calculate the weighted mean elevation to get the z-component
-        elevation_values = self.ds['elevation'].where(cells_within_radius, drop=True)
-        weighted_z = (elevation_values * cell_areas).sum() / cell_areas.sum()
-
-        # Combine components to form the cap center vector
-        cap_center_vector = np.array([weighted_x.item(), weighted_y.item(), weighted_z.item()])
-
-        # The radius of the cap is the length of the cap center vector
-        cap_radius = np.linalg.norm(cap_center_vector)
-
-        return cap_center_vector, cap_radius
-
-# Example usage:
-#cap_center_vector, cap_radius = sim.target.calculate_cap_orientation(sim.crater.location, sim.crater.final_radius)
-    
-
-    
