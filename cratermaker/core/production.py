e6946aaf1c8df1dda378b46b042d3baaae4b82aa
David Minton <daminton@purdue.edu>
2023-12-11 12:06:12 -0500

    Improving the documentation and readability of the NPF model. Also added in projectile coefficients

--- cratermaker/core/production.py
+++ cratermaker/core/production.py
@@ -58,15 +58,69 @@ class NeukumProductionFunction():
             raise ValueError("target_name must be a string")
         
         # The Neukum production function for the Moon and Mars
-        #Lunar PF from: Ivanov, Neukum, and Hartmann (2001) SSR v. 96 pp. 55-86
-        #Mars PF from: Ivanov (2001) SSR v. 96 pp. 87-104
+        # Lunar PF from: Neukum, G., Ivanov, B.A., Hartmann, W.K., 2001. Cratering Records in the Inner Solar System in Relation to 
+        #    the Lunar Reference System. Space Science Reviews 96, 55–86. https://doi.org/10.1023/A:1011989004263
+        # Mars PF from: Ivanov, B.A., 2001. Mars/Moon Cratering Rate Ratio Estimates. Space Science Reviews 96, 87–104.
+        #    https://doi.org/10.1023/A:1011941121102
+        # Projectile PF from: Ivanov, B.A., Neukum, G., Wagner, R., 2001. Size-Frequency Distributions of Planetary Impact Craters 
+        #    and Asteroids, in: Collisional Processes in the Solar System. Springer Netherlands, Dordrecht, pp. 1–34. 
+        #    https://doi.org/10.1007/978-94-010-0712-2_1
+
+
         sfd_coef = {
-                "Moon" : [-3.0876,-3.557528,+0.781027,+1.021521,-0.156012,-0.444058,+0.019977,+0.086850,-0.005874,-0.006809,+8.25e-4, +5.54e-5],
-                "Mars" : [-3.384, -3.197,   +1.257,   +0.7915,  -0.4861,  -0.3630,  +0.1016,  +6.756e-2,-1.181e-2,-4.753e-3,+6.233e-4,+5.805e-5]
+                "Moon" : np.array(
+                    [
+                        -3.0876,
+                        -3.557528,
+                        +0.781027,
+                        +1.021521,
+                        -0.156012,
+                        -0.444058,
+                        +0.019977,
+                        +0.086850,
+                        -0.005874,
+                        -0.006809,
+                        +8.25e-4, 
+                        +5.54e-5
+                    ]),
+                "Mars" : np.array(
+                [
+                    -3.384, 
+                    -3.197,
+                    +1.257,
+                    +0.7915,
+                    -0.4861,
+                    -0.3630,
+                    +0.1016,
+                    +6.756e-2,
+                    -1.181e-2,
+                    -4.753e-3,
+                    +6.233e-4,
+                    +5.805e-5
+                ]),
+                "Projectile" : np.array(
+                [
+                    0,
+                    +1.375458,
+                    +1.272521e-1,
+                    -1.282166,
+                    -3.074558e-1,
+                    +4.149280e-1,
+                    +1.910668e-1,
+                    -4.260980e-2,
+                    -3.976305e-2,
+                    -3.180179e-3,
+                    +2.799369e-3,
+                    +6.892223e-4,
+                    +2.614385e-6,
+                    -1.416178e-5,
+                    -1.191124e-6
+                ]
+                )
             }
         sfd_range = {
-            "Moon" : [0.01,1000],
-            "Mars" : [0.015,362]
+            "Moon" : np.array([0.01,1000]),
+            "Mars" : np.array([0.015,362]),
         }
        
         
@@ -92,12 +146,12 @@ class NeukumProductionFunction():
              check_valid_time: bool=True
              ) -> Union[float_like, np.ndarray]:
         """
-        Return the cumulative size-frequency distribution form of the production function over a given time range
+        Return the cumulative size-frequency distribution of craters over a given time range and crater diameters.
 
         Parameters
         ----------
         diameter : float_like or numpy array
-            Crater diameter(s) to compute corresponding cumulative N values in units of m
+            Crater diameter(s) in units of meters to compute corresponding cumulative number density value.
         time_range : Tuple of 2 values, default=(-1000.0,0.0)
             time range relative to the present day to compute cumulative SFD in units of My. Defaults to the last 1 
         check_valid_time : bool, optional (default=True)
@@ -111,29 +165,78 @@ class NeukumProductionFunction():
             
         Notes
         ----- 
+        The CSFD is computed using the model of Ivanov, Neukum, and Hartmann (2001) SSR v. 96 pp. 55-86 for the Moon and Mars, with 
+        minor changes. Notably, there is a typo in the chronology function (Eq. 5) of the original paper. The linear term in the paper
+        is given as 8.38e-4. The value should be 10**(a0), and therefore the number given in the paper is based on the "Old"
+        coefficients from Neukum (1983). The correct value is 10**(-3.0876) = 8.17e-4. We compute the value from the coefficients 
+        in our implementation of the chronology function.
         """
        
         if time_range[0] > time_range[1]:
             raise ValueError("time_range[0] must be less than time_range[1]")
          
         Dkm = diameter * 1e-3           # Convert m to km for internal functions
-        time_Gy = -time_range * 1e-3    # Convert time range from My to Gy ago for internal functions
+        time_Gy = -np.array(time_range) * 1e-3    # Convert time range from My to Gy ago for internal functions
         
-        N0 = self.CSFD(Dkm) * self.time_to_N(time_Gy[0],check_valid_time) * 1e6  
-        N1 = self.CSFD(Dkm) * self.time_to_N(time_Gy[1],check_valid_time) * 1e6 
+        N0 = self._CSFD(Dkm) * self._time_to_Nrel(time_Gy[0],check_valid_time)
+        N1 = self._CSFD(Dkm) * self._time_to_Nrel(time_Gy[1],check_valid_time)
          
         return (N0 - N1)*1e6 # Convert from km^-2 to m^-2
+   
     
     def csfd_to_time(self,
                      diameter: float_like | Sequence[float_like] | np.ndarray,
                      csfd: float_like | Sequence[float_like] | np.ndarray,
+                     reference_time: float_like | Sequence[float_like] | np.ndarray | None = None,
                      check_valid_time: bool=True
                      ) -> Union[float_like, np.ndarray]:
         """
-        Return the time in the past for the given cumulative size-frequency distribution of craters as a function of diameter.
+        Return the time in My relative to a reference time given a cumulative number density of craters of a given diameter. 
         
-    
+        Parameters
+        ----------
+        diameter : float_like or numpy array
+            Crater diameter(s) in units of meters to compute corresponding time values. 
+        csfd: float_like or numpy array
+            Cumulative number density of craters per square meter greater than the input diameter. 
+            `csfd and `diameter` must have the same shape.
+        reference_time : optional, default=None
+            Reference time in My from present to compute time values relative to. If None, the reference time is set to 0.0 (the present)
+        check_valid_time : bool, optional (default=True)
+            If True, return NaN for time values outside the valid time range
+
+        Returns
+        -------
+        float_like or numpy array of float_like
+            The time values in My relative to the present day 
         """
+        # Check if both diameter and csfd are scalars or arrays of the same shape
+        if np.isscalar(diameter) and np.isscalar(csfd):
+            pass
+        elif np.shape(diameter) == np.shape(csfd):
+            pass
+        else:
+            raise ValueError("diameter and csfd must be both scalars or arrays of the same shape")
+        
+        if reference_time is None:
+            reference_time = 0.0
+        else:
+            reference_time -= 1e-3 # convert from My from present to Gy ago
+        
+        if check_valid_time and reference_time > self.max_time:
+            raise ValueError(f"reference_time must be less than {self.max_time} Gy ago")
+           
+        Dkm = diameter * 1e-3 # convert from m to km 
+        Nrel = csfd * 1e-6 / self._CSFD(Dkm) # convert from m^-2 to km^-2 and get the number density relative to the reference diameter
+        Nref = self._time_to_Nrel(reference_time,check_valid_time) 
+        time_Gy = self._Nrel_to_time(Nrel-Nref,check_valid_time)
+        
+        if check_valid_time:
+            time_Gy = np.where(time_Gy <= self.max_time, time_Gy, np.nan)
+            
+        time_My = -time_Gy * 1e3 # convert from Gy ago to My from present
+        
+        return time_My.item() if np.isscalar(diameter) else time_My
 
 
     def _N1(self,
@@ -180,12 +283,12 @@ class NeukumProductionFunction():
         """
         def _CSFD_scalar(Dkm):
             if Dkm < self.sfd_range[0]:
-                slope = self.dNdD(self.sfd_range[0])
-                A = self.CSFD(self.sfd_range[0])
+                slope = self._dNdD(self.sfd_range[0])
+                A = self._CSFD(self.sfd_range[0])
                 return A * (Dkm / self.sfd_range[0]) ** slope
             elif Dkm > self.sfd_range[1]:
-                slope = self.dNdD(self.sfd_range[1])
-                A = self.CSFD(self.sfd_range[1])
+                slope = self._dNdD(self.sfd_range[1])
+                A = self._CSFD(self.sfd_range[1])
                 return A * (Dkm / self.sfd_range[1]) ** slope
             else:
                 logCSFD = sum(co * np.log10(Dkm) ** i for i, co in enumerate(self.sfd_coef))
@@ -315,10 +418,10 @@ if __name__ == "__main__":
             'Mars': CSFDfig.add_subplot(122)}
 
         tvals = [0.01,1.0,4.0]
-        x_min = 1e-5
+        x_min = 1e-3
         x_max = 1e4
         y_min = 1e-12
-        y_max = 1e7
+        y_max = 1e6
         nD = 1000
         Dvals = np.logspace(np.log10(x_min), np.log10(x_max), num=nD)
         for key in ax:
@@ -340,12 +443,13 @@ if __name__ == "__main__":
             lo = Dvals < npf.sfd_range[0]
             hi = Dvals > npf.sfd_range[1]
             for t in tvals:
-                prod = npf.production_csfd(t,Dvals)
+                prod = npf.csfd(diameter=Dvals*1e3,time_range=(-t*1e3,0.0))
+                prod *= 1e-6 # convert from m^-2 to km^-2
                 ax[key].plot(Dvals[inrange], prod[inrange], '-', color='black', linewidth=1.0, zorder=50)
-                ax[key].plot(Dvals[lo], prod[lo], '-.', color='orange', linewidth=1.0, zorder=50)
-                ax[key].plot(Dvals[hi], prod[hi], '-.', color='orange', linewidth=1.0, zorder=50)
-                labeli = int(0.5*nD)
-                ax[key].text(Dvals[labeli],prod[labeli],f"{t:.2f} ", ha="left", va="top",rotation=-72)
+                ax[key].plot(Dvals[lo], prod[lo], '-.', color='orange', linewidth=2.0, zorder=50)
+                ax[key].plot(Dvals[hi], prod[hi], '-.', color='orange', linewidth=2.0, zorder=50)
+                labeli = int(0.25*nD)
+                ax[key].text(Dvals[labeli],3*prod[labeli],f"{t:.2f} ", ha="left", va="top",rotation=-72)
 
         plt.tick_params(axis='y', which='minor')
         plt.tight_layout()
@@ -355,15 +459,13 @@ if __name__ == "__main__":
         fig = plt.figure(1, figsize=(8, 4))
         ax = fig.add_subplot(111)
         ax.set_yscale('log')
-        ax.set_ylabel('$\mathregular{N_{1} (km^{-2})}$')
-        ax.set_xlabel('Time ()')
+        ax.set_ylabel('$\mathregular{N(1) (km^{-2})}$')
+        ax.set_xlabel('Time (Gy ago)')
         ax.set_xlim(4.5, 0)
         #ax.set_ylim(1e-1, 1e5)
         npf = NeukumProductionFunction(target_name="Moon")
         tvals = np.linspace(4.5, 0.0, num=1000)
-        v_N1 = np.vectorize(npf.N1)
-        N1 = v_N1(tvals)
-        ax.plot(tvals, N1, '-', color='black', linewidth=1.0, zorder=50)
+        ax.plot(tvals, npf._N1(tvals), '-', color='black', linewidth=1.0, zorder=50)
         plt.tight_layout()
         plt.show() 
         
