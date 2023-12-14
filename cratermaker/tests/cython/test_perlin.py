9c01ee8f55f5714734b467fdb4ce9c6a03b0518a
David Minton <daminton@purdue.edu>
2023-12-14 15:59:49 -0500

    Fixed typo and added in the test that was missing due to .gitignore

---
+++ cratermaker/tests/cython/test_perlin.py
@@ -0,0 +1,11 @@
+import unittest
+from cratermaker.perlin import *
+
+class TestPerlin(unittest.TestCase):
+
+    def test_perlin(self):
+        pass
+        
+
+if __name__ == '__main__':
+    unittest.main()
