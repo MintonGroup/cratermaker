08bfe8733d5a2e29cec72ab3b3347e85b5320c77
David Minton <daminton@purdue.edu>
2025-11-05 20:08:55 -0500

    Made some minor changes to the Mars HiResLocal example

--- examples/01-simulation_and_visualization/mars_simulation_pyvista.py
+++ examples/01-simulation_and_visualization/mars_simulation_pyvista.py
@@ -6,7 +6,8 @@ Run a simulation of a local region on Mars.
 
 This example demonstrates how to use the HiResLocal Surface to run a crater population over a small region of a planet, in this case Mars.
 
-For this example, we will The simulation uses the default Mars configuration and runs for 1 billion years on a local region with a radius of 20 km and resolution of 100 m/pix.  We then visualize the surface using PyVista and a Mars-like colormap.
+For this example, we will The simulation uses the default Mars configuration and runs for 2 billion years on a local region with a radius of 20 km and resolution of 100 m/pix.  We then visualize the surface using PyVista and a Mars-like colormap.
+
 
 """
 
@@ -15,11 +16,21 @@ import pyvista as pv
 import cratermaker as cm
 
 # Run a simple Mars sim
-sim = cm.Simulation(target="Mars", surface="hireslocal", local_location=(0, 0), pix=100.0, local_radius=20.0e3, ask_overwrite=False)
-sim.run(age=1000)
+sim = cm.Simulation(
+    target="Mars",
+    surface="hireslocal",
+    local_location=(0, 0),  # Equator and Prime Meridian
+    pix=100.0,
+    local_radius=20.0e3,
+    ask_overwrite=False,
+    rng_seed=86186233407,  # This will ensure we get the same crater population each time we run the example
+)
+sim.run(age=2000)
 
 sim.export(format="vtp")
 mesh = pv.read(sim.surface.output_dir / "local_surface000001.vtp")
 plotter = pv.Plotter()
-plotter.add_mesh(mesh, scalars="face_elevation", cmap="Oranges", show_edges=False)
+plotter.add_mesh(mesh, scalars="face_elevation", cmap="pink", show_edges=False)
+# Orient the camera to look down on the surface
+plotter.camera_position = "yz"
 plotter.show()
