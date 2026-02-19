c9d5e7f733a0288f8e87555d8eb6fac06cfc275e
David Minton <daminton@purdue.edu>
2026-02-19 18:10:50 -0500

    Updated docs and added example showing the new compute_location_from_distance_bearing function. Also used built-in dark theme for PyVista plotter. Updated changelog.

---
+++ examples/01-simulation_and_visualization/1.6-emplace_crater_at_distance.py
@@ -0,0 +1,34 @@
+"""
+Emplace a crater a specific distance and bearing from the center of a HiResLocalSurface
+=======================================================================================
+
+This example shows how to emplace a crater at a specific distance and bearing from the center of a HiResLocalSurface using the new `compute_location_from_distance_bearing` function of the Surface class. This is useful to place a crater on a HiResLocalSurface at a specific location relative to the center without trying to figure out what lat,lon coordinates to use.
+
+"""
+
+import cratermaker as cm
+
+simdir = "simdata-1_6"
+
+# Note, that for these examples we use cratermaker's cleanup function to prepare a fresh directory for the example to run. This will
+# prevent prompts that will prevent these examples from running on their own when building the documentation pages. Alternatively,
+# passing ask_overwrite=False and reset=True to Simulation will also allow the example to run without requiring any prompts.
+cm.cleanup(simdir)
+
+sim = cm.Simulation(
+    surface="hireslocal",
+    simdir="craterstats_compare_lores",
+    local_location=(0, 0),
+    pix=10.0,
+    local_radius=2500.0,
+    ask_overwrite=False,
+    reset=True,
+    rng_seed=349572341965,
+)
+
+# Put a 500 m crater 1 km due north of the center
+location = sim.surface.local.compute_location_from_distance_bearing(distances=1000.0, bearings=0)
+sim.emplace(diameter=500.0, location=location)
+
+# Use the "c" key to show the circle representing the crater
+sim.show(variable_name="face_elevation", cmap="cividis")
