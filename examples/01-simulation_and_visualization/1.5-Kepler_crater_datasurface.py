3ce69b6c6aca3d3f0278abdb9c7727bb2b1d90cb
David Minton <daminton@purdue.edu>
2026-03-13 10:02:08 -0400

    Reverted the datasurface examples back to the original now that the Readthedocs time limit has been increased from 30 to 55 min.

--- examples/01-simulation_and_visualization/1.5-Kepler_crater_datasurface.py
+++ examples/01-simulation_and_visualization/1.5-Kepler_crater_datasurface.py
@@ -10,34 +10,20 @@ This example demonstrates how to use the DataSurface to fetch real DEM data for 
 
 import os
 
-# This example can take some time due to the use of DataSurface, which must download data from the NASA PDS. For document building, we will simple display a pre-rendered image rather than running the script to save resources.
 simdir = "simdata-1_5"
-if not os.environ.get("READTHEDOCS", False):
-    from cratermaker import Surface
+from cratermaker import Surface
 
-    # Note, that for these examples we pass ask_overwrite=False and reset=True to the Simulation constructor. This will suppress
-    # prompts that ask the user if they want to overwrite existing files, which would would prevent these examples from running on their
-    # own when building the documentation pages. Alternatively, calling cm.cleanup(simdir) will remove all pre-existing output files.
-    surface = Surface.maker(
-        "datasurface",
-        local_location=(321.9913, 8.121),
-        local_radius=50.0e3,
-        pix=200.0,
-        simdir=simdir,
-        ask_overwrite=False,
-        reset=True,
-    )
-    if os.environ.get("RENDERFIGS", False):
-        surface.plot(plot_style="hillshade", show=False, save=True)
-    surface.show3d(superdomain=False)
-    surface.show3d(superdomain=True)
-
-else:
-    from pathlib import Path
-
-    import matplotlib.image as mpimg
-    import matplotlib.pyplot as plt
-
-    plt.imshow(mpimg.imread(Path(simdir) / "surface_images" / "local_surface_hillshade.png"))
-    plt.axis("off")
-    plt.show()
+# Note, that for these examples we pass ask_overwrite=False and reset=True to the Simulation constructor. This will suppress
+# prompts that ask the user if they want to overwrite existing files, which would would prevent these examples from running on their
+# own when building the documentation pages. Alternatively, calling cm.cleanup(simdir) will remove all pre-existing output files.
+surface = Surface.maker(
+    "datasurface",
+    local_location=(321.9913, 8.121),
+    local_radius=50.0e3,
+    pix=200.0,
+    simdir=simdir,
+    ask_overwrite=False,
+    reset=True,
+)
+surface.show3d(superdomain=False)
+surface.show3d(superdomain=True)
