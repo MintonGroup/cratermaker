d676a852c1f4d732e9c9c15ba0be54bd613ce0ad
leo-minton <leo.a.minton@gmail.com>
2026-03-26 14:08:58 -0400

    Start progress on the UI

--- crates/cratermaker-gui/src/loader.rs
+++ crates/cratermaker-gui/src/loader.rs
@@ -1,4 +1,10 @@
-use pyo3::{intern, prelude::*, types::PyType};
+use std::{collections::HashMap, sync::Arc};
+
+use pyo3::{
+    intern,
+    prelude::*,
+    types::{PyFunction, PyType},
+};
 
 #[derive(Clone)]
 pub struct Inspect<'py> {
@@ -47,12 +53,12 @@ pub fn get_indexable_members<'py>(
 }
 
 #[derive(Debug)]
-pub struct Cratermaker<'py> {
-    pub module: Module<'py>,
+pub struct Cratermaker {
+    pub module: Module,
 }
 
-impl<'py> Cratermaker<'py> {
-    pub fn load(py: Python<'py>) -> PyResult<Self> {
+impl Cratermaker {
+    pub fn load(py: Python<'_>) -> PyResult<Self> {
         let cratermaker = py.import("cratermaker")?;
         Ok(Cratermaker {
             module: Module::load(&Inspect::load(py), cratermaker),
@@ -60,37 +66,17 @@ impl<'py> Cratermaker<'py> {
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct Module<'py> {
+#[derive(Debug)]
+pub struct Module {
     pub name: String,
     pub docstring: Option<String>,
-    pub inner: Bound<'py, PyModule>,
-    pub submodules: Vec<Module<'py>>,
-    pub classes: Vec<Class<'py>>,
+    pub inner: Py<PyModule>,
+    pub submodules: HashMap<String, Module>,
+    pub classes: HashMap<String, Arc<Class>>,
 }
 
-#[derive(Debug, Clone)]
-pub struct Class<'py> {
-    pub name: String,
-    pub docstring: Option<String>,
-    pub inner: Bound<'py, PyType>,
-}
-
-impl<'py> Class<'py> {
-    pub fn load(inspect: &Inspect, class: Bound<'py, PyType>) -> Self {
-        let name = class.name().unwrap().extract::<String>().unwrap();
-        let docstring = inspect.getdoc(class.as_any());
-        let members = get_indexable_members(inspect, class.as_any());
-        Self {
-            name,
-            docstring,
-            inner: class,
-        }
-    }
-}
-
-impl<'py> Module<'py> {
-    pub fn load(inspect: &Inspect<'py>, module: Bound<'py, PyModule>) -> Self {
+impl Module {
+    pub fn load<'py>(inspect: &Inspect<'py>, module: Bound<'py, PyModule>) -> Self {
         let mut name = module.name().unwrap().extract::<String>().unwrap();
         let docstring = inspect.getdoc(module.as_any());
         let members = get_indexable_members(inspect, module.as_any());
@@ -101,26 +87,102 @@ impl<'py> Module<'py> {
                 submodule
                     .name()
                     .unwrap()
-                    .extract::<&str>()
+                    .to_str()
                     .unwrap()
                     .starts_with(&name)
             })
             .map(|submodule| Module::load(inspect, submodule.clone()))
+            .map(|submodule| (submodule.name.clone(), submodule))
             .collect();
         let classes = members
             .iter()
             .filter_map(|(_, item)| item.cast::<PyType>().ok())
-            .map(|class| Class::load(inspect, class.clone()))
+            .filter(|class| {
+                class
+                    .fully_qualified_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .starts_with(&name)
+            })
+            .map(|class| Arc::new(Class::load(inspect, class.clone())))
+            .map(|class| (class.name.clone(), class))
             .collect();
         if let Some((_, shortname)) = name.rsplit_once('.') {
             name = shortname.to_string();
         }
         Self {
-            inner: module,
+            inner: module.unbind(),
             docstring,
             submodules,
             name,
             classes,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Class {
+    pub name: String,
+    pub docstring: Option<String>,
+    pub inner: Py<PyType>,
+    pub methods: HashMap<String, Method>,
+    pub create_method: Method,
+}
+
+impl Class {
+    pub fn load<'py>(inspect: &Inspect<'py>, class: Bound<'py, PyType>) -> Self {
+        let name = class.name().unwrap().extract::<String>().unwrap();
+        let docstring = inspect.getdoc(class.as_any());
+        let members = get_indexable_members(inspect, class.as_any());
+        let mut methods: HashMap<String, Method> = members
+            .iter()
+            .filter_map(|(name, item)| item.cast::<PyFunction>().ok().map(|item| (name, item)))
+            .map(|(name, method)| {
+                (
+                    name.clone(),
+                    Method::load(inspect, method.as_any().clone(), name.clone()),
+                )
+            })
+            .collect();
+        let create_method = methods.remove("maker").unwrap_or_else(|| {
+            let mut method = Method::load(
+                inspect,
+                class
+                    .getattr(intern!(class.py(), "__init__"))
+                    .unwrap()
+                    .cast_into()
+                    .unwrap(),
+                "__init__".to_string(),
+            );
+            method.inner = class.clone().unbind().into_any();
+            method
+        });
+
+        Self {
+            name,
+            docstring,
+            inner: class.unbind(),
+            methods,
+            create_method,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Method {
+    pub name: String,
+    pub docstring: Option<String>,
+    pub inner: Py<PyAny>,
+}
+
+impl Method {
+    pub fn load<'py>(inspect: &Inspect<'py>, method: Bound<'py, PyAny>, name: String) -> Self {
+        let docstring = inspect.getdoc(&method);
+        Self {
+            name,
+            docstring,
+            inner: method.unbind(),
         }
     }
 }
--- crates/cratermaker-gui/src/main.rs
+++ crates/cratermaker-gui/src/main.rs
@@ -1,14 +1,110 @@
 mod loader;
 
+use std::sync::Arc;
+
+use iced::{
+    Element, Length,
+    widget::{button, column, container, row, text},
+};
 use pyo3::prelude::*;
 
-use crate::loader::Cratermaker;
+use crate::loader::{Class, Cratermaker};
+
+struct Variable {
+    name: String,
+    class: Arc<Class>,
+    value: Py<PyAny>,
+}
+
+impl Variable {
+    fn view(&self) -> Element<'_, Message> {
+        container(row![
+            container(text(&self.name)).align_left(Length::Fill),
+            container(text(&self.class.name).style(text::secondary)).align_right(Length::Fill)
+        ])
+        .style(container::rounded_box)
+        .into()
+    }
+}
+
+#[derive(Default)]
+struct VariablePane {
+    variables: Vec<Variable>,
+}
+
+impl VariablePane {
+    fn view(&self) -> Element<'_, Message> {
+        column(self.variables.iter().map(|variable| variable.view()))
+            .spacing(6)
+            .into()
+    }
+}
+
+struct Toolbar;
+
+impl Toolbar {
+    fn view(&self) -> Element<'_, Message> {
+        row![button("new").on_press(Message::CreateObject)].into()
+    }
+}
+
+struct App {
+    cratermaker: Cratermaker,
+    variable_pane: VariablePane,
+    toolbar: Toolbar,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    CreateObject,
+}
+
+impl App {
+    fn run() -> iced::Result {
+        iced::run(Self::update, Self::view)
+    }
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::CreateObject => {
+                let class = self
+                    .cratermaker
+                    .module
+                    .submodules
+                    .get("components")
+                    .unwrap()
+                    .submodules
+                    .get("crater")
+                    .unwrap()
+                    .classes
+                    .get("Crater")
+                    .unwrap()
+                    .clone();
+
+                let value = Python::attach(|py| class.create_method.inner.call0(py).unwrap());
+
+                self.variable_pane.variables.push(Variable {
+                    name: "new object".to_string(),
+                    class,
+                    value,
+                })
+            }
+        }
+    }
+    fn view(&self) -> Element<'_, Message> {
+        column![self.toolbar.view(), self.variable_pane.view()].into()
+    }
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Python::attach(|py| Self {
+            cratermaker: Cratermaker::load(py).unwrap(),
+            variable_pane: Default::default(),
+            toolbar: Toolbar,
+        })
+    }
+}
 
 fn main() {
-    Python::attach(|py| -> PyResult<()> {
-        let cratermaker = Cratermaker::load(py)?;
-        println!("{:#?}", cratermaker);
-        Ok(())
-    })
-    .unwrap();
+    App::run().unwrap()
 }
