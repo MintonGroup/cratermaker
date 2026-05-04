Crater.maker() does not use the projectile_density argument as it should. Fixes #128. Morphology now has a component of Scaling so that it doesn't have to be passed explicitly to internal Crater.maker calls.

cratermaker/components/surface/hireslocal.py
@@ -424,7 +424,6 @@ class HiResLocalSurface(Surface):
 
     def set_superdomain(
         self,
-        scaling: Scaling | str | None = None,
         morphology: Morphology | str | None = None,
         reset: bool = False,
         regrid: bool = False,
@@ -437,8 +436,6 @@ class HiResLocalSurface(Surface):
 
         Parameters
         ----------
-        scaling : Scaling | str | None, optional
-            The scaling model to use. If None, the default scaling model will be used.
         morphology : Morphology | str | None, optional
             The morphology model to use. If None, the default morphology model will be used.
         reset : bool, optional
@@ -452,11 +449,10 @@ class HiResLocalSurface(Surface):
 
         from cratermaker import Crater
 
-        scaling = Scaling.maker(scaling, target=self.target, **kwargs)
         morphology = Morphology.maker(morphology, surface=self, target=self.target, **kwargs)
 
         antipode_distance = np.pi * self.target.radius
-        projectile_velocity = scaling.projectile.mean_velocity * 10
+        projectile_velocity = morphology.scaling.projectile.mean_velocity * 10
 
         distance = 1.0
         dvals = []
@@ -468,10 +464,9 @@ class HiResLocalSurface(Surface):
                 np.log10(self.target.radius * 2),
                 1000,
             ):
-                crater = Crater.maker(
+                crater = morphology.Crater.maker(
                     diameter=diameter,
                     angle=90.0,
-                    scaling=scaling,
                     projectile_velocity=projectile_velocity,
                 )
                 rmax = morphology.rmax(crater=crater, minimum_thickness=1e-3)
@@ -495,7 +490,7 @@ class HiResLocalSurface(Surface):
             self._superdomain_function_exponent = 1.0
         self._superdomain_scale_factor = self.superdomain_function(antipode_distance)
 
-        self._load_from_files(reset=reset, regrid=regrid, scaling=scaling, morphology=morphology, **kwargs)
+        self._load_from_files(reset=reset, regrid=regrid, **kwargs)
         return
 
     def set_face_proj(self):

cratermaker/core/simulation.py
@@ -211,6 +211,7 @@ class Simulation(CratermakerBase):
             surface=self.surface,
             production=self.production,
             counting=self.counting,
+            scaling=self.scaling,
             **morphology_config,
         )
 
@@ -218,7 +219,6 @@ class Simulation(CratermakerBase):
         # This is because when creating a new Surface object of this type, the grid generation is deferred until the Scaling and Morphology objects are initialized in order to set the superdomain properly.
         if issubclass(self.surface.__class__, HiResLocalSurface) and self.surface.uxgrid is None:
             self.surface.set_superdomain(
-                scaling=self.scaling,
                 morphology=self.morphology,
                 reset=self.is_new,
                 **surface_config,
@@ -786,7 +786,6 @@ class Simulation(CratermakerBase):
                     self.Crater.maker(
                         location=location,
                         time=time,
-                        scaling=self.scaling,
                         **diam_arg,
                         **vars(self.common_args),
                         **kwargs,
@@ -862,8 +861,6 @@ class Simulation(CratermakerBase):
             sim.emplace(craters)
 
         """
-        if craters is None and "scaling" not in kwargs:
-            kwargs["scaling"] = self.scaling
         self.is_new = False
         return self.morphology.emplace(craters=craters, **kwargs)
 
@@ -1435,7 +1432,6 @@ class Simulation(CratermakerBase):
                 projectile_diameter=projectile_diameter,
                 angle=angle,
                 projectile_velocity=projectile_velocity,
-                scaling=scaling,
                 **vars(self.common_args),
             ).diameter
             if limit == "smallest":
@@ -1452,7 +1448,6 @@ class Simulation(CratermakerBase):
                 diameter=crater_diameter,
                 angle=angle,
                 projectile_velocity=projectile_velocity,
-                scaling=scaling,
                 **vars(self.common_args),
             ).projectile_diameter
             return float(projectile_diameter)
@@ -1628,7 +1623,10 @@ class Simulation(CratermakerBase):
             return self.production.diameter_range[0]
         elif self.production.generator_type == "projectile":
             projectile_diameter = self.production.diameter_range[0]
-            return self._get_diameter_limit("smallest", projectile_diameter=projectile_diameter)
+            if projectile_diameter > 0:
+                return self._get_diameter_limit("smallest", projectile_diameter=projectile_diameter)
+            else:
+                return projectile_diameter
 
     @smallest_crater.setter
     def smallest_crater(self, value):
@@ -1659,7 +1657,10 @@ class Simulation(CratermakerBase):
             return self.production.diameter_range[1]
         elif self.production.generator_type == "projectile":
             projectile_diameter = self.production.diameter_range[1]
-            return self._get_diameter_limit("largest", projectile_diameter=projectile_diameter)
+            if not np.isinf(projectile_diameter):
+                return self._get_diameter_limit("largest", projectile_diameter=projectile_diameter)
+            else:
+                return projectile_diameter
 
     @largest_crater.setter
     def largest_crater(self, value):