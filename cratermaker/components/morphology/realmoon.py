Fixed normalization of psd profile and added in psd1d_num_points as an argument so that PSD computations can be adjusted for resolution

cratermaker/components/morphology/realmoon.py
@@ -50,6 +50,7 @@ class RealMoonCraterVariable(MorphologyCraterVariable):
         floor_radius_control: np.ndarray | None = None,
         wall_texture_control: np.ndarray | None = None,
         ejecta_texture_control: np.ndarray | None = None,
+        psd1d_num_points: int | None = None,
         **kwargs: Any,
     ) -> None:
         super().__init__(**kwargs)
@@ -59,6 +60,7 @@ class RealMoonCraterVariable(MorphologyCraterVariable):
         object.__setattr__(self, "_floor_radius_control", None)
         object.__setattr__(self, "_wall_texture_control", None)
         object.__setattr__(self, "_ejecta_texture_control", None)
+        object.__setattr__(self, "_psd1d_num_points", psd1d_num_points)
         return
 
     @property
@@ -102,6 +104,16 @@ class RealMoonCraterVariable(MorphologyCraterVariable):
         The control points for the ejecta texture PSD.
         """
         return self._ejecta_texture_control
+
+    @property
+    def psd1d_num_points(self) -> int:
+        """
+        The number of points used in the construction of the 1D PSD.
+        """
+        if self._psd1d_num_points is None:
+            return _PSD1D_NUM_POINTS
+        else:
+            return self._psd1d_num_points
 
 
 @Crater.register("realmooncrater")
@@ -311,7 +323,7 @@ class RealMoonCrater(BasicMoonCrater):
         """
         return realmoon_bindings.get_1d_psd_from_control_points(
             control_points=self.rim_radius_control,
-            npoints=_PSD1D_NUM_POINTS,
+            npoints=self.psd1d_num_points,
             add_noise=self.morphology.add_noise,
             rng_seed=self.rim_radius_rng_seed,
         )
@@ -323,7 +335,7 @@ class RealMoonCrater(BasicMoonCrater):
         """
         return realmoon_bindings.get_1d_psd_from_control_points(
             control_points=self.rim_flank_radius_control,
-            npoints=_PSD1D_NUM_POINTS,
+            npoints=self.psd1d_num_points,
             add_noise=self.morphology.add_noise,
             rng_seed=self.rim_flank_radius_rng_seed,
         )
@@ -335,7 +347,7 @@ class RealMoonCrater(BasicMoonCrater):
         """
         return realmoon_bindings.get_1d_psd_from_control_points(
             control_points=self.floor_radius_control,
-            npoints=_PSD1D_NUM_POINTS,
+            npoints=self.psd1d_num_points,
             add_noise=self.morphology.add_noise,
             rng_seed=self.floor_radius_rng_seed,
         )
@@ -347,7 +359,7 @@ class RealMoonCrater(BasicMoonCrater):
         """
         return realmoon_bindings.get_1d_psd_from_control_points(
             control_points=self.rim_elevation_control,
-            npoints=_PSD1D_NUM_POINTS,
+            npoints=self.psd1d_num_points,
             add_noise=self.morphology.add_noise,
             rng_seed=self.rim_elevation_rng_seed,
         )